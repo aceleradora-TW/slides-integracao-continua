ec19847054437f49789104cd21ba1fbf291b0d56
othman853 <anuar.yasser@gmail.com>
2017-05-03 21:35:59 -0300

    Including images in the build output

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ rm -rf _static
 rm -rf ./build
 mkdir -p ./build
 cp -r ./node_modules/reveal.js/{css,js,lib,plugin} ./build
-cp -r ./theme ./build
+cp -r ./{theme,images} ./build
 ./node_modules/.bin/reveal-md slides.md --theme thoughtworks --static
 cp -r _static/* ./build/
 rm -rf _static
